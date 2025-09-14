edc47aa919e55d6d2581dfea3fcd501411033bb2
Irina Ivanova <irina.ivanova.bis@gmail.com>
2025-09-14 20:59:34 +0400

    add 2 number in operations (+ - *)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        int sum = number1 + number2;
+        int difference = number1 - number2;
+        int product = number1 * number2;
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
     }
 }
